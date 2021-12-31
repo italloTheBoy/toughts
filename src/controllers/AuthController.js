d0d820d9af727a6b47ccb58e3d9b7b0496871479
itallo <itallomkspop@gmail.com>
2021-12-30 22:43:06 -0300

    register

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
 module.exports = class AuthController {
   static async login(req, res) {
     res.render('auth/login')
@@ -5,5 +8,67 @@ module.exports = class AuthController {
 
   static async register(req, res) {
     res.render('auth/register')
+  }
+
+  static async registerPost(req, res) {
+    const name = req.body.name.toLowerCase().trim()
+    const email = req.body.email.toLowerCase().trim()
+    const password = req.body.password.toLowerCase()
+    const confirmPassword = req.body.confirmPassword.toLowerCase().trim()
+
+
+    let err = false
+
+    if (!name || name === '') {
+      req.flash('nameErr', 'Insira um nome')
+      err = true
+    }
+
+    if (!email || email === '') {
+      req.flash('emailErr', 'Insira um email')
+      err = true
+    }
+    else {
+      await User.findOne({ where: { email } })
+        .then(user => {
+          if (user) {
+            req.flash('emailErr', 'Email já cadastrado')
+            err = true
+          }
+        })
+        .catch(err => {
+          console.log(err)
+          return res.status(500).redirect('/500')
+        })
+    }
+
+    if (!password || password === '') {
+      req.flash('passwordErr', 'Insira uma senha')
+      err = true
+    }
+    else if (password !== confirmPassword) {
+      req.flash('passwordErr', 'As senhas não batem')
+      err = true
+    }
+
+    if (err === true) return res.status(403).render('auth/register')
+
+
+    const hash = await bcrypt.genSalt(15).then(salt => {
+      return bcrypt.hash(password, salt)
+    })
+
+    await User.create({
+      name,
+      email,
+      password: hash,
+    })
+    .catch(err => {
+      console.log(err)
+      return res.status(500).redirect('/500')
+    })
+
+
+    return res.redirect('/')
   }
 }
